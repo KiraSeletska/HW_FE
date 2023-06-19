acb8db5b428f21e2c931f7da0b0ed2fd46e84006
KiraSeletska <117731125+KiraSeletska@users.noreply.github.com>
2023-06-19 15:47:25 +0200

    Add files via upload

---
+++ layout_app_11.07/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { Header } from './components/Header'
+import { Main } from './components/Main'
+import { Services } from './components/Services';
+import { Clients } from './components/Clients';
+import { EmployeesList } from './components/EmployeesList';
+import { Footer } from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Services />
+      <Clients />
+      <EmployeesList />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
